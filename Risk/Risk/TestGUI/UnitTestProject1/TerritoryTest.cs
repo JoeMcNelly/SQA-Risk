b1f3ecf8349b2b1a1c45637c3e9e89a7dd6fcd58
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-04-15 16:30:12 -0400

    added first test for Territory Equals method
    because I started making this method without TDD these commits are going
    to seem a bit wonky but it will even out at the end (hopefully)

---
+++ Risk/Risk/TestGUI/UnitTestProject1/TerritoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using TestGUI;
+
+namespace UnitTestProject1
+{
+    [TestFixture()]
+    public class TerritoryTest
+    {
+        [Test()]
+        public void TestTerritoryEqualsEmpty()
+        {
+            Territory target = new Territory();
+            Territory test = new Territory();
+
+            Assert.IsTrue(target.Equals(test));
+        }
+    }
+}
