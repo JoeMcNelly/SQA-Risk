92193ff576643a6a0e287c489bb3c96806e14126
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-04-23 05:40:59 -0400

    added new map class and a test for its initalization

---
+++ Risk/TestRisk/MapTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestGUI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MapTest
+    {
+        [TestMethod]
+        public void TestMapInitalizes()
+        {
+            Map target = new Map();
+
+            Assert.IsNotNull(target);
+        }
+    }
+}
