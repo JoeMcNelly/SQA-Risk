9f4b1c0cdf532afccb366e7ffa24e366b456e1dc
seanmmcpherson <mcphersm@rose-hulman.edu>
2015-05-01 10:43:34 -0400

    sort of...
    forgot to push randomized cards back onto Deck stack; test fails

--- Risk/TestRisk/GameTest.cs
+++ Risk/TestRisk/GameTest.cs
@@ -529,6 +529,7 @@ namespace TestRisk
 
             List<List<Card>> beforeAndAfterLists = game.shuffleDeck();
             Assert.AreNotEqual(beforeAndAfterLists[0], beforeAndAfterLists[1]);
+            Assert.AreNotEqual(0, game.getDeck().Count);
         }
 
     }
